3735c090702cc8c290dbf8930223ff794c80775a
Matt <total_garbage_inovernivneriv8934@mailinator.com>
2015-08-03 17:27:18 +1000

    Correct the HTTP status from GET / - it should be 300 (Multiple Choices) not 200. Implement the details of a given version.

--- versionsapp.py
+++ versionsapp.py
@@ -1,4 +1,5 @@
 from webob import Response
+import webob.exc
 from apiversion import APIVersion
 from application import Application
 from apiv1app import APIv1App
@@ -7,22 +8,30 @@ class VersionsApp(Application):
 
 	version_classes = [ APIv1App ]
 
+	def _api_version_detail(self, version):
+		return {
+			"id": version._version_identifier(),
+			"links": [
+				{
+					"href": "/" + version._version_identifier(),
+					"rel": "self"
+				}
+			]
+		}
+
 	def APIVersionList(self, args):
-		return Response(content_type = 'application/json', body = self._resultset_to_json([
-			{
-				"id": version._version_identifier(),
-				"links": [
-					{
-						"href": "/" + version._version_identifier(),
-						"rel": "self"
-					}
-				]
-			} for version in self.version_classes
+		return Response(status = 300, content_type = 'application/json', body = self._resultset_to_json([
+			self._api_version_detail(version) for version in self.version_classes
 		]))
 
-	def APIVersion(self, args):
+	def APIVersion(self, version_identifier):
+		versions = [ version for version in self.version_classes if version._version_identifier() == version_identifier ]
+		if not versions:
+			return webob.exc.HTTPNotFound()
+		if len(versions) > 1:
+			raise RuntimeError("Multiple API versions with identifier '%s'" % version_identifier)
 		return Response(content_type = 'application/json', body = self._resultset_to_json({
-			'todo': 'Report detail'
+			self._api_version_detail(versions[0])
 		}))
 
 def factory(global_config, **settings):
