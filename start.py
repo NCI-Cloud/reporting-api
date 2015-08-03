dea2e9b3f50666198a92fe0f39d3da46e553c911
Matt <total_garbage_inovernivneriv8934@mailinator.com>
2015-08-04 03:14:33 +1000

    Fix the Python start script.

--- start.py
+++ start.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python
 
 import sys, os
-from paste.deploy import loadapp
+from paste.deploy import loadapp, loadserver
+from paste import httpserver
 
-reporting_app = loadapp('config:paste.config', relative_to=os.path.dirname(os.path.realpath(__file__)))
+if __name__ == '__main__':
+	paste_config = 'paste.config'
+	reporting_app = loadapp('config:' + paste_config, relative_to=os.path.dirname(os.path.realpath(__file__)))
+	server = loadserver('config:' + paste_config, relative_to=os.path.dirname(os.path.realpath(__file__)))
+	server(reporting_app)
