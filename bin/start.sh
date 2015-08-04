aa3b815a4ae94103e2bd8649809bedb05c3e5a1c
Matt <total_garbage_inovernivneriv8934@mailinator.com>
2015-08-04 15:16:35 +1000

    Move the paste config file into a 'conf' subdir

--- bin/start.sh
+++ bin/start.sh
@@ -3,4 +3,4 @@
 script=$(readlink -f "$0")
 bindir=$(dirname "$script")
 
-PYTHONPATH="$bindir"/.. paster serve paste.config --reload >> /var/log/reporting-api.log 2>&1
+PYTHONPATH="$bindir"/.. paster serve "$bindir/../conf/paste.config" --reload >> /var/log/reporting-api.log 2>&1
