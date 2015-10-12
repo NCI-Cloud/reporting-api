a6a2ee870840730f99ad475e02956c49fe2e7ed3
Matt <total_garbage_inovernivneriv8934@mailinator.com>
2015-10-12 13:34:37 +1100

    Remove hardcoded default filename. Raise an error if no app config file was specified, or it is unreadable, or it doesn't contain the section we need.

--- common/authapp.py
+++ common/authapp.py
@@ -8,6 +8,8 @@ class KeystoneApplication(Application):
     An Application which uses Keystone for authorisation using RBAC
     """
 
+    INI_SECTION = 'keystone_authtoken'
+
     def __init__(self, configuration):
         super(KeystoneApplication, self).__init__(configuration)
         self.required_role = self.config.get('authorisation', 'required_role')
@@ -22,8 +24,11 @@ class KeystoneApplication(Application):
 
 def keystone_auth_filter_factory(global_config, **local_config):
     global_config.update(local_config)
-    config_file_name = global_config.get('config_file', 'apiv1app.ini')
+    config_file_name = global_config.get('config_file')
+    if not config_file_name:
+        raise ValueError('No config_file directive')
     config_file = ConfigParser.SafeConfigParser()
-    config_file.read(config_file_name)
-    global_config.update(config_file.items('keystone_authtoken'))
+    if not config_file.read(config_file_name):
+        raise ValueError("Cannot read config file '%s'" % config_file_name)
+    global_config.update(config_file.items(KeystoneApplication.INI_SECTION))
     return auth_filter_factory(global_config)
