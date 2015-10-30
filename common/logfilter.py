4d010e95692f5ff44c998a5c401bd567b0f7ecf1
Matt <total_garbage_inovernivneriv8934@mailinator.com>
2015-10-30 18:56:50 +0900

    Remove unused file.

--- common/logfilter.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-from paste.translogger import TransLogger
-
-def factory(config, **settings):
-	def filter(app):
-		config.update(settings);
-		return TransLogger(app, setup_console_handler = True)
-	return filter
