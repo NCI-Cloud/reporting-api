adb0bffd6586fa380a077f1ec0b950c6ae5d8b4f
Matt <total_garbage_inovernivneriv8934@mailinator.com>
2015-11-10 15:18:46 +1100

    Make the confdir option optional, with default value the conf subdir in the source tree. Wrap long lines. Add a -l alias for option --logfile.

--- bin/reporting-api.py
+++ bin/reporting-api.py
@@ -11,10 +11,27 @@ import logging
 import argparse
 
 def parse_args():
+    REALFILE = os.path.realpath(__file__)
+    REALDIR = os.path.dirname(REALFILE)
+    PARDIR = os.path.realpath(os.path.join(REALDIR, os.pardir))
+    CONFDIR = os.path.join(PARDIR, 'reporting_api', 'conf')
     parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--confdir', action='store', required=True, help="Specify config directory")
-    parser.add_argument('--logfile', action='store', required=False, default='/var/log/reporting-api.log',
-                        help="Specify the file to log to")
+    parser.add_argument(
+        '-c',
+        '--confdir',
+        action='store',
+        required=False,
+        default=CONFDIR,
+        help="Specify config directory"
+    )
+    parser.add_argument(
+        '-l',
+        '--logfile',
+        action='store',
+        required=False,
+        default='/var/log/reporting-api.log',
+        help="Specify the file to log to"
+    )
     return parser.parse_args()
 
 if __name__ == '__main__':
