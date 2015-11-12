64e8787072956e6845cba11543fe588146bcec24
Simon Fowler <simon.fowler@anu.edu.au>
2015-11-12 13:51:48 +1100

    Create a pidfile.
    Configurable via the --pidfile option.

--- bin/reporting-api.py
+++ bin/reporting-api.py
@@ -4,18 +4,27 @@
 Start the Reporting API application using Paste Deploy.
 """
 
-import sys
 import os
 from paste.deploy import loadapp, loadserver
 import logging
 import argparse
 
+
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--confdir', action='store', required=True, help="Specify config directory")
-    parser.add_argument('--logfile', action='store', required=False, default='/var/log/reporting-api.log',
+    parser.add_argument('-c', '--confdir', action='store', required=True,
+                        help="Specify config directory")
+    parser.add_argument('--logfile', action='store', required=False,
+                        default='/var/log/reporting-api.log',
                         help="Specify the file to log to")
+    parser.add_argument('--pidfile', action='store', required=False,
+                        default='/var/run/reporting-api.pid')
     return parser.parse_args()
+
+
+def create_pidfile(pidfile="/var/run/reporting-api.pid"):
+    with open(pidfile, 'rw') as pf:
+        pf.write("%d" % (os.getpid()))
 
 if __name__ == '__main__':
     args = parse_args()
@@ -25,5 +34,5 @@ if __name__ == '__main__':
     PASTE_CONFIG = os.path.join(args.confdir, 'paste.config')
     REPORTING_APP = loadapp('config:' + PASTE_CONFIG)
     SERVER = loadserver('config:' + PASTE_CONFIG)
+    create_pidfile(args.pidfile)
     SERVER(REPORTING_APP)
-
